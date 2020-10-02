888f08cb5ef789e1a3e2921af7775c14e550c58d
Yanuar Aditia <yanuaraditia@outlook.com>
2020-10-02 17:01:09 +0700

    Suavecito

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,15 @@ export default {
     plugins: [
         '@plugins/vuetify'
     ],
-    components: true
+    components: true,
+    head: {
+        titleTemplate: '%s - Nuxt.js',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      
+            // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+            { hid: 'description', name: 'description', content: 'Meta description' }
+        ]
+    }
  }
